b5bdbd7cdffbc1b561c7826847d98beae96b7d4d
Jessica <jess.newman19@gmail.com>
2022-05-18 11:15:55 -0400

    commit tree b9ea95fe070b223c6abca5ace07fd95e47d76b73

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3 
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind 
+    if length1 == length2 && length2 == length3 
+      :equilateral
+    elsif length1 == length2 || length2 == length3 || length1 == length3
+      :isosceles
+    else 
+      :scalene
+    end
+  end 
+
+  def sides_greater_than_zero? 
+    [length1, length2, length3].all? {|length| length.positive?}
+  end 
+
+  def valid_triangle_inequality?
+    length1 + length2 > length3 && length1 + length3 > length2 && length2 + length3 > length1
+  end
+  
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError 
+  end
+binding.pry
 end
